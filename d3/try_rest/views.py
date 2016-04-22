1785b8cc57c8651fe550cb2f76510a87eb558846
Mathew Zaleski <matz@zaleski.ca>
2016-04-22 13:31:46 -0400

    clean up some commented out garbage code

--- d3/try_rest/views.py
+++ d3/try_rest/views.py
@@ -7,20 +7,22 @@ from django.db.models.query import QuerySet
 from try_rest.serializers import PersonSerializer
 from try_mysql.models import Person
 
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
+
+
 class LegacyPersonListRestView(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    
+
+# evidently POST requests are meant just for create. this works for post, put, patch
+
 class LegacyPersonDetailsRestView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PersonSerializer
     lookup_field = "first_name"
+    permission_classes = (IsAdminUser, IsAuthenticated, )
     def get_queryset(self):
         thingy = self.kwargs["first_name"]
         print("LegacyPersonDetailsRestView returning", thingy)
-        #how does this parse?? how does python know what employee_number is??
+        #TODO: learn python so as to know how this parses?? how does python know what employee_number is??
         return Person.objects.filter(first_name=thingy)
-    
-
-    
-
-    
